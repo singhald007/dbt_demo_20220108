6251cc8ed3c802954999a10f6b9e05702bfca921
Deepak Singhal <deepak.singhal@hotmail.com>
2023-10-05 13:01:21 +0000

    commit

--- models/TPC_H_01_STAGE_VIEWS/stg_customer_vw.sql
+++ models/TPC_H_01_STAGE_VIEWS/stg_customer_vw.sql
@@ -1,12 +1,19 @@
 /* Marerized as table or view */
-{{ config(materialized='view') }}
+{{ config(materialized="view") }}
 
-with source_data as (
-select C_CUSTKEY,C_NAME,C_ADDRESS,C_NATIONKEY,C_PHONE,C_ACCTBAL,C_MKTSEGMENT,C_COMMENT 
-from "SNOWFLAKE_SAMPLE_DATA"."TPCH_SF1"."CUSTOMER"
-)
+with
+    source_data as (
+        select
+            c_custkey,
+            c_name,
+            c_address,
+            c_nationkey,
+            c_phone,
+            c_acctbal,
+            c_mktsegment,
+            c_comment
+        from "SNOWFLAKE_SAMPLE_DATA"."TPCH_SF1"."CUSTOMER"
+    )
 
 select *
 from source_data
-
-
---
+++ models/costco/stg_customer.sql
@@ -0,0 +1,16 @@
+with
+    source_data as (
+        select
+            c_custkey,
+            c_name,
+            c_address,
+            c_nationkey,
+            c_phone,
+            c_acctbal,
+            c_mktsegment,
+            c_comment
+        from "SNOWFLAKE_SAMPLE_DATA"."TPCH_SF1"."CUSTOMER"
+    )
+
+select *
+from source_data
